1abd2d11e9e5187433d14865f5563c4a9225fe3a
Alain Belair <belair.alain@gmail.com>
2021-09-21 16:40:19 -0400

    fix map options
    - use browser language default
    - use quarterly default version
    - change property visibility inJsLoad

--- src/Utils/JsLoader.php
+++ src/Utils/JsLoader.php
@@ -20,26 +20,28 @@ class JsLoader
     public static $version = '2.0.0';
 
     /** @var bool */
-    public static $isLoaded = false;
+    private static $isLoaded = false;
 
     /** @var string The google api developer key. */
-    public static $apiKey = '';
+    protected static $apiKey = '';
 
     /** @var string Google maps version. */
-    public static $apiVerstion = '3.46';
+    protected static $apiVerstion = 'quarterly';
 
     /** @var string[] Libraries to load with Google api. */
-    public static $apiLibraries = ['places'];
+    protected static $apiLibraries = ['places'];
 
-    /** @var string */
-    public static $language = 'en';
-
-    /** @var array Google Map options */
-    public static $mapOptions = [];
+    /** @var array Google Map options as per https://googlemaps.github.io/js-api-loader/interfaces/LoaderOptions.html */
+    protected static $mapOptions = [];
 
     public static function setGoogleApiKey(string $key): void
     {
         self::$apiKey = $key;
+    }
+
+    public static function setMapOptions(array $options): void
+    {
+        self::$mapOptions = $options;
     }
 
     /**
@@ -70,7 +72,6 @@ class JsLoader
                 'apiKey' => self::$apiKey,
                 'version' => self::$apiVerstion,
                 'libraries' => self::$apiLibraries,
-                'language' => self::$language,
             ], self::$mapOptions)));
 
             self::$isLoaded = true;
